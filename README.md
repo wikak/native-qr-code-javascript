d2d17a7f7c5e365f9b9813d50d45ee15b03c76b7
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-28 08:57:28 +0000

    repo init

---
+++ README.md
@@ -0,0 +1,1 @@
+# native-qr-code-javascript
---
+++ main.js
@@ -0,0 +1,21 @@
+function openQRCamera(node) {
+  console.log("openQRCamera");
+  var reader = new FileReader();
+  reader.onload = function() {
+    node.value = "";
+    qrcode.callback = function(res) {
+      if(res instanceof Error) {
+        alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
+      } else {
+        node.parentNode.previousElementSibling.value = res;
+      }
+    };
+    qrcode.decode(reader.result);
+  };
+  reader.readAsDataURL(node.files[0]);
+}
+
+function showQRIntro() {
+  console.log("showQRIntro");
+  return confirm("Use your camera to take a picture of a QR code.");
+}
